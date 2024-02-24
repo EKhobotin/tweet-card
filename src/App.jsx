0f9337f979b461d30e99c63df1b20b870d271e6a
EKhobotin <evgen.29.3.85@gmail.com>
2024-02-24 12:21:33 +0200

    test gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>tweet-card</p>
+        <p>tweet-card-test-page</p>
       </header>
     </div>
   );
